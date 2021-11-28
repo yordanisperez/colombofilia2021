638d43cf4aefeb7ede2e0f9d907171b208349587
Yordanis Perez Brito <yordanis@gmail.com>
2021-11-28 13:30:49 -0500

    Add Procfile to proyect

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 
@@ -6,7 +5,7 @@ const path = require('path');
 
 
   
-const port = 8080;
+const port = process.env.PORT|8080;
 
 
 app.use(express.urlencoded({ extended: true })); //Usar el bodyparser que enta 
